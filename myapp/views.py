5db6923a7591ead7e72c4a93217bf5f5b9379cac
Carlos Paniagua <55609769+carlos-paniagua@users.noreply.github.com>
2023-06-27 22:28:43 +0900

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,37 +1,40 @@
 from django.http import JsonResponse
-from django.views import View
-from .models import Event,Participant
+from django.core import serializers
+from django.shortcuts import get_object_or_404
+from .models import Event, Participant
 
-#イベント情報表示画面
-class EventDetailsView(View):
-    #GET
-    def get(self, request, *args, **kwargs):
-        
-        #イベントIDを取得
-        event_id = self.kwargs.get('event_id')
-        
-        #イベントをデータベースから探す
-        event = Event.objects.filter(id=event_id).first()
-        
-        #イベントがない：エラー出力
-        if event is None:
-            return JsonResponse({'エラー': '指定されたイベントは存在しません。'})
-        
-        #参加者をデータベースから取得
-        participants = Participant.objects.filter(event=event)
-        
-        #データ作成JSON形式
-        data = {
-            'イベント名': event.name,
-            '日付': event.date.isoformat(),
-            '支払総額': str(event.total_amount),
-            '徴収額': str(event.total_amount / participants.count()),
-            '参加者': [{
-                'ユーザー名': participant.user.username,
-                '預かった金額': str(participant.paid_amount),
-                '返す金額': str(participant.paid_amount - (event.total_amount / participants.count())),
-            } for participant in participants]
+#保存イベント一覧：
+def event_list(request):
+    events = Event.objects.all()
+    event_list = []
+    for event in events:
+        event_list.append({
+            'id': event.id,
+            'date': event.date.strftime('%Y/%m/%d'),
+            'name': event.name
+        })
+    return JsonResponse(event_list, safe=False)
+
+#イベント詳細情報
+def event_detail(request, event_id):
+    event = get_object_or_404(Event, pk=event_id)  
+    participants = Participant.objects.filter(event=event)
+    participant_list = []
+    
+    #データJSON形式
+    for participant in participants:
+        participant_data = {
+            'name': participant.user.username,
+            'paid_amount': str(participant.paid_amount),
+            'collection_amount': str(participant.collection_amount),
+            'return_amount': str(participant.return_amount)
         }
-        
-        #JSON形式でクライアントに返す
-        return JsonResponse(data, safe=False)
+        participant_list.append(participant_data)
+    event_data = {
+        'event_name': event.name,
+        'date': event.date.strftime('%Y/%m/%d'),
+        'participants': participant_list
+    }
+    #JSON形式でクライアントに返す
+    return JsonResponse(event_data, safe=False)
+
