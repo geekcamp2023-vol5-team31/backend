91f70cda51ebc9c96a8749c7ce8c0a3ff2baca0a
hayato2475 <gto15723@gmail.com>
2023-06-27 22:54:40 +0900

    JSONFieldの修正

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('data/', views.EventDetailsView.as_view(), name='data')
+    path('event_list/', views.event_list, name='event_list'),
+    path('event_detail', views.event_detail, name='event_detail')
 ]
