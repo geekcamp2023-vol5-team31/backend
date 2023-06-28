c52279e235e728c920fa64f4f50b78a19c2c6202
carlos-paniagua <carlos_paniagua0421@hotmail.com>
2023-06-28 23:47:07 +0900

    update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('event_list/', views.event_list, name='event_list'),
+    path('save/<int:user_id>', views.save_data, name='save_data'),
+    path('event_list/<int:user_id>', views.event_list, name='event_list'),
     path('events/<int:event_id>/', views.event_detail, name='event_detail'), 
 ]
