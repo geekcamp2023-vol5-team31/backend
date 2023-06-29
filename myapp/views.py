516ef9532e4e5c090133350637432d12792a56d8
Carlos Paniagua <carlos_paniagua0421@hotmail.com>
2023-06-29 10:30:36 +0900

    ユーザ認証を修正　コメント追加

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,5 @@
 from django.http import JsonResponse
-from django.shortcuts import get_object_or_404
-from .models import Event, Participant
+from .models import Event
 import requests,json
 
 #データ保存用
@@ -12,24 +11,29 @@ def save_data(request,user_id):
         if not user_id:
             return JsonResponse({'error': 'ユーザが認証失敗'}, status=401)
         
-        json_data = request.POST.get('data') #フロントから渡されるデータ
+        json_data = json.loads(request.body) #フロントから渡されるデータ
         data = json.loads(json_data)         #json解析
         Event.objects.create(data=data)      #データベースに保存
         return JsonResponse({'success': True})  #成功レンスポンス
     
 #保存イベント一覧：
 def event_list(request, user_id):
-    auth_token = get_github_user_id(request.META.get("HTTP_AUTHORIZATION"))
-    access_token = auth_token.split(' ')[1]
-    user_id = get_github_user_id(access_token)
-    events = Event.objects.all()
+    # ユーザ認証
+    user_id = get_github_user_id(user_id)
+    
+    #↓おかしい？
+    # auth_token = get_github_user_id(request.META.get("HTTP_AUTHORIZATION"))
+    # access_token = auth_token.split(' ')[1]
+    # user_id = get_github_user_id(access_token)
+    events = Event.objects.all()#Event取り出す
     event_list = []
-    for event in events:
+    for event in events:#user_idと一致するか
         if event.user == user_id:
             event_list.append(event.data)
-    return JsonResponse(event_list, safe=False)
+            
+    return JsonResponse(event_list, safe=False)#イベントリストを返す
 
-#github api
+#githubユーザ認証
 def get_github_user_id(access_token):
     headers = {
         'Authorization': f'Bearer {access_token}',
@@ -39,6 +43,6 @@ def get_github_user_id(access_token):
     if response.status_code == 200:
         user_data = response.json()
         user_id = user_data['id']
-        return user_id
+        return user_id  #user_idを返す
     else:
         return None
