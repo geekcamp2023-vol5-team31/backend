aa3fc1b1267b9be3e8cbce032efb18f8d2d9c0b2
carlos-paniagua <carlos_paniagua0421@hotmail.com>
2023-06-29 23:23:23 +0900

    view.py update

--- myapp/views.py
+++ myapp/views.py
@@ -22,12 +22,16 @@ def save_data(request):
     if request.method == 'POST':
         # ユーザ認証
         auth_token = request.META.get("HTTP_AUTHORIZATION")
-        user_id = get_github_user_id(auth_token.split("")[1])
+        user_id = get_github_user_id(auth_token.split(" ")[1])
+        
+        #ユーザがないときuser_id新規作成してデータ保存
         if not user_id: 
             json_data = request.POST.get('data')
             data = json.loads(json_data)
             Event.objects.create(user=user_id, data=data)
             return JsonResponse({'success': True})
+        
+        #ある時データ保存
         else:
             json_data = request.POST.get('data') #フロントから渡されるデータ
             data = json.loads(json_data)         #json解析
@@ -37,12 +41,9 @@ def save_data(request):
 #保存イベント一覧：
 def event_list(request, user_id):
     # ユーザ認証
-    user_id = get_github_user_id(user_id)
+    auth_token = get_github_user_id(request.META.get("HTTP_AUTHORIZATION"))
+    user_id = get_github_user_id(auth_token.split(' ')[1])
     
-    #↓おかしい？
-    # auth_token = get_github_user_id(request.META.get("HTTP_AUTHORIZATION"))
-    # access_token = auth_token.split(' ')[1]
-    # user_id = get_github_user_id(access_token)
     events = Event.objects.all()#Event取り出す
     event_list = []
     for event in events:#user_idと一致するか
