57d2dfc73aad64a0f3b6812dddddc25b3549447a
carlos-paniagua <carlos_paniagua0421@hotmail.com>
2023-07-01 16:00:54 +0900

    get_events get_event_detail追加

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #追加分
+    path('get_events/', views.get_events, name='get_events'),
+    path('get_event_detail/', views.get_event_detail, name='get_event_detail'),
+    
     path('event_list/', views.event_list, name='event_list'),
     path('save_data/', views.save_data, name='save_data'), 
     path('csrf_token/', views.csrf_token, name="csrf_token")
--- myapp/views.py
+++ myapp/views.py
@@ -2,23 +2,57 @@ from django.http import JsonResponse
 from .models import Event
 import requests,json
 from django.middleware.csrf import get_token
+from django.core import serializers
 
 #githubユーザ認証
-def get_github_user_id(access_token):
-    headers = {
-        'Authorization': f'Bearer {access_token}',
-        'Accept': 'application/vnd.github.v3+json'
-    }
-    response = requests.get('https://api.github.com/user', headers=headers)
-    if response.status_code == 200:
-        user_data = response.json()
-        user_id = user_data['id']
-        return user_id  #user_idを返す
+def get_github_user_id(request,access_token):
+    if 'user_id' in request.session:
+        return request.session['user_id']
     else:
-        return None
+        headers = {
+            'Authorization': f'Bearer {access_token}',
+            'Accept': 'application/vnd.github.v3+json'
+        }
+        response = requests.get('https://api.github.com/user', headers=headers)
+        if response.status_code == 200:
+            user_data = response.json()
+            user_id = user_data['id']
+            request.session['user_id'] = user_id #セッションにuser_idを保存
+            return user_id  #user_idを返す
+        else:
+            return None
 
 def csrf_token(request):
     return JsonResponse({"token": get_token(request)})
+
+# イベント一覧を取得
+def get_events(request):
+    auth_token = request.META.get("HTTP_AUTHORIZATION")
+    user_id = get_github_user_id(auth_token.split(" ")[1])
+    
+    #データベースからユーザーに関連するイベントを取得
+    events = Event.objects.filter(user=user_id).values('id', 'data')
+    event_list = []
+    for event in events:
+        event_list.append({
+            'id': event['id'],
+            'data': event['data']
+        })
+    return JsonResponse(event_list, safe=False)
+
+# イベントの詳細を取得
+def get_event_detail(request, event_id):
+    auth_token = request.META.get("HTTP_AUTHORIZATION")
+    user_id = get_github_user_id(auth_token.split(" ")[1])
+    
+    #データベースから対応するイベントを取得
+    event = Event.objects.filter(user=user_id, id=event_id).first()
+    #ある場合データを返す
+    if event:
+        return JsonResponse(event.data)
+    #ない場合：エラー
+    else:
+        return JsonResponse({'error': 'Event not found'}, status=404)
 
 #データ保存用
 def save_data(request):
